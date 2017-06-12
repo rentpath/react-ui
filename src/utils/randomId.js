18acdc050f9bf2979f82d8a7cc897b0aba9fd2b7
Ari Bouius <abouius@rentpath.com>
2017-06-12 15:45:03 -0700

    Add utility functions

--- src/utils/randomId.js
+++ src/utils/randomId.js
@@ -11,7 +11,11 @@ const randomStr = () => (
  * Generates a random alphanumeric ID.
  *
  * @param {string} [prefix] An optional prefix.
+<<<<<<< 9df21285badbdbc2a7cf1a7e98e86e5be8f59fec
  * @return {string}
+=======
+ * @return {string{
+>>>>>>> Add utility functions
  */
 export default prefix => (
   `${prefix || randomStr()}-${Math.floor(Math.random() * 0xFFFF)}`.replace(/[^a-z0-9-_]/gi)
