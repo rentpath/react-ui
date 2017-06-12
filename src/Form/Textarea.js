ae0166a72ddfe1c66fc78d7a2cf20d7027659786
Ari Bouius <abouius@rentpath.com>
2017-06-12 15:52:40 -0700

    Add form/input components

---
+++ src/Form/Textarea.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import themed from 'react-themed'
+
+@themed('Textarea', {
+  pure: true,
+})
+
+export default class Textarea extends Component {
+  static propTypes = {
+    theme: PropTypes.object,
+    className: PropTypes.string,
+  }
+
+  static defaultProps = {
+    theme: {},
+  }
+
+  render() {
+    const {
+      theme,
+      className,
+      ...props
+    } = this.props
+
+    return (
+      <textarea
+        {...props}
+        className={classNames(
+          className,
+          theme.Textarea,
+        )}
+      />
+    )
+  }
+}
