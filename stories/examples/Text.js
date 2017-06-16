75a6a8b2a4e149c4e380eceb42788c414a8620e3
Tad Johnston <tjohnston@rentpath.com>
2017-06-16 12:36:55 -0700

    adding storybook to repo

---
+++ stories/examples/Text.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TextTheme } from '../theme'
+import { Text } from '../../src'
+
+export default (
+  <Text
+    theme={TextTheme}
+  >
+    Some example text
+  </Text>
+)
