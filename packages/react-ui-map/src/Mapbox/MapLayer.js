774f4c99257344422dce28e33a6b12796350ea57
Arkodeb Maity <maityarkodeb@gmail.com>
2017-08-04 10:33:42 -0400

    Integrate Map Layers and Sources
    [Story](https://www.pivotaltracker.com/story/show/149846374)
    
    This is a new branch following the restructuring of react-ui-map.

---
+++ packages/react-ui-map/src/Mapbox/MapLayer.js
@@ -0,0 +1,84 @@
+import React, { Component, PropTypes } from 'react'
+import merge from 'lodash.merge'
+
+MapLayer.contextTypes = {
+  map : PropTypes.object.isRequired
+}
+
+MapLayer.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  sourceLayer: PropTypes.string.isRequired,
+  sourceId: PropTypes.string.isRequired,
+  paint: PropTypes.object.isRequired,
+  layout: PropTypes.object.isRequired,
+  before: PropTypes.string.isRequired,
+  layerOpacity: PropTypes.isRequired,
+  layerVisibility: PropTypes.isRequired
+}
+
+class MapLayer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    const { map } = this.context;
+    const {
+      id,
+      type,
+      sourceLayer,
+      sourceId,
+      layout = {},
+      paint = {},
+      layerOpacity,
+      layerVisibility,
+      before
+    } = this.props
+
+    const layerId = `${sourceId}-${id}`
+    const opacity = `${type}-opacity`
+
+    map.addLayer({
+      id: layerId,
+      source: sourceId,
+      'source-layer': sourceLayer,
+      type,
+      layout,
+      paint: merge(paint, {
+        [opacity]: layerOpacity
+      })
+    }, before)
+
+    if(!layerVisibility) map.setLayoutProperty(layerId, 'visibility', 'none')
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { map } = this.context
+    const { id, type, sourceId, layerOpacity, layerVisibility } = this.props
+    const layerId = `${sourceId}-${id}`
+
+    if (nextProps.layerOpacity && nextProps.layerOpacity !== layerOpacity) {
+      map.setPaintProperty(layerId, `${type}-opacity`, nextProps.layerOpacity)
+    }
+    if (nextProps.layerVisibility !== layerVisibility) {
+      const visibility = (nextProps.layerVisibility) ? 'visible' : 'none'
+      map.setLayoutProperty(layerId, 'visibility', visibility)
+    }
+
+    return null;
+  }
+
+  componentWillUnmount() {
+    const { map } = this.context
+    const { id, sourceId } = this.props
+    const layerId = `${sourceId}-${id}`
+    map.removeLayer(layerId)
+  }
+
+  render() {
+    return null
+  }
+}
+
+export default MapLayer
---
+++ packages/react-ui-map/src/Mapbox/MapSource.js
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react'
+
+MapSource.contextTypes = {
+  map : PropTypes.object.isRequired
+}
+
+MapSource.propTypes = {
+  id: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  layer: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+}
+
+class MapSource extends React.Component {
+  constructor(props) {
+      super(props)
+  }
+
+  componentWillMount() {
+    const { map } = this.context;
+    const {
+      id,
+      url
+    } = this.props
+    map.addSource(id, { type: 'vector', url })
+  }
+
+  componentWillUnmount() {
+    const { map } = this.context
+    const { id } = this.props
+    map.removeSource(id)
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children &&
+          React.Children.map(this.props.children, child => (
+            React.cloneElement(child, {
+              sourceId: this.props.id,
+              sourceLayer: this.props.layer
+            })
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default MapSource
