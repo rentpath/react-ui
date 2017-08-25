dd424ee4808b57eeea701491bf5abd94260e5850
Arkodeb Maity <maityarkodeb@gmail.com>
2017-08-25 12:55:32 -0400

    add some tests

---
+++ packages/react-ui-map/src/Mapbox/__tests__/Layer-test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Layer from '../Layer'
+
+describe('<Layer />', () => {
+  it('renders with a layer', () => {
+    const props = {
+      layer: {"foo": "bar"},
+    }
+    const context = {
+      map: {
+        addLayer: () => true,
+      },
+    }
+    const wrapper = mount(<Layer {...props} />, { context })
+    expect(wrapper.prop('layer')).toEqual({"foo": "bar"})
+  })
+})
