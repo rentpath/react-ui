6c1819de8aec0731c4c03838e41c7457f104c4fc
Yaw Asabere <yasabere@gmail.com>
2017-12-05 12:04:44 -0500

    mend
    added suggestionValueSelector

--- stories/examples/Searchbar.js
+++ stories/examples/Searchbar.js
@@ -22,11 +22,18 @@ export const DefaultSearchBar = (
 )
 
 export const SearchBarWithHTMLButton = (
-  <SearchBar
+  [<SearchBar
     theme={theme}
     placeholder="Search"
     resetButton={<button>X</button>}
     value="test"
     suggestions={[<h1>HELLO</h1>, 'a', 'c', 'd']}
-  />
+    suggestionValueSelector={value => {
+      if (typeof value === 'object') {
+        return value.props.children
+      }
+      return value
+    }}
+  />,
+  ]
 )
