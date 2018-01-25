817ef5399b6f0f586840a5265d0c702f13fc8714
Ghamoti Anye Angwafo <gangwafo@rentpath.com>
2018-01-25 17:02:02 -0500

    fix(Updated Babel preset from ES2015 to env for react-ui-core): mapbox async/await calls werent gett
    affects: @rentpath/react-ui-ag, @rentpath/react-ui-core
    
    This babel-preset-env has ES2015+ to ES{currentyear}-stage 3 features. It should
    stop us from
    needing to include other plugins that are already stage 3. However, it helps us with adding the
    async/await plugin
    since it includes ES2017 completed features.

---
+++ packages/react-ui-map/src/Mapbox/index.js
@@ -0,0 +1,5 @@
+// export { default } from './Mapbox'
+// export { default as Mapbox } from './Mapbox'
+// export { default as MapLayerInteractions } from './MapLayerInteractions'
+// export { default as MapSource } from './MapSource'
+// export { default as MapLayer } from './MapLayer'
