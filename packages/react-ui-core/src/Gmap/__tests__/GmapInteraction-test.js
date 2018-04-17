c18339bfd7c5610a73275e2bb464199c1e4870a8
Orion Free <ofree@upperquadrant.com>
2018-04-16 20:29:51 -0400

    Adding tests for deeper call and Marker Interactions

--- packages/react-ui-core/src/Gmap/__tests__/GmapInteraction-test.js
+++ packages/react-ui-core/src/Gmap/__tests__/GmapInteraction-test.js
@@ -2,9 +2,10 @@ import { GmapInteraction } from '../GmapInteraction'
 
 describe('GmapInteraction', () => {
   const onZoom = jest.fn()
+  const addGeoJson = jest.fn()
 
   it('registers the map', () => {
-    const map = { onZoom }
+    const map = { onZoom, data: { addGeoJson } }
     const registerSpy = jest.spyOn(GmapInteraction, 'registerMap')
     GmapInteraction.registerMap(map)
 
@@ -25,5 +26,33 @@ describe('GmapInteraction', () => {
     const args = { foo: 'bar' }
     GmapInteraction.call('onZoom', args)
     expect(onZoom).toHaveBeenCalledWith(...args)
+  })
+
+  it('invokes a deep call', () => {
+    const args = { foo: 'bar' }
+    GmapInteraction.call('data.addGeoJson', args)
+    expect(addGeoJson).toHaveBeenCalledWith(...args)
+  })
+})
+
+describe('MarkerInteraction', () => {
+  it('adds a marker to the map', () => {
+    const markerSpy = jest.spyOn(window.google.maps, 'Marker')
+    const result = GmapInteraction.MarkerInteraction.setupMarker({})
+
+    expect(markerSpy).toHaveBeenCalled()
+    expect(result).toEqual({})
+  })
+  it('removes a marker', () => {
+    const setMap = jest.fn()
+    const eventSpy = jest.spyOn(
+      window.google.maps.event,
+      'clearInstanceListeners'
+    )
+    const marker = { id: 'markerId', setMap }
+    GmapInteraction.MarkerInteraction.removeMarker(marker)
+
+    expect(eventSpy).toHaveBeenCalledWith(marker)
+    expect(marker.setMap).toHaveBeenCalledWith(null)
   })
 })
