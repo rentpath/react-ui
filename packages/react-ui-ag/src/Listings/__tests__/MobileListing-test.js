b459d609bda9cae29b261703087a0d4077472c87
Tad Johnston <tjohnston@rentpath.com>
2018-04-19 10:18:55 -0700

    fix(ag listing): theme needs destructured
    affects: @rentpath/react-ui-ag
    
    Fixes theme prop being passed down accidently causing the theme to break
    for the children

--- packages/react-ui-ag/src/Listings/__tests__/MobileListing-test.js
+++ packages/react-ui-ag/src/Listings/__tests__/MobileListing-test.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
-import { omit } from 'lodash'
-import { Button, ToggleButton, ListingComponents } from '@rentpath/react-ui-core'
-import ThemedMobileListing from '../Listing'
+import { mount } from 'enzyme'
+import { ListingComponents } from '@rentpath/react-ui-core'
+import ThemedMobileListing from '../MobileListing'
 import theme from './mocks/theme'
 
 const MobileListing = ThemedMobileListing.WrappedComponent
@@ -81,172 +80,92 @@ const props = {
   lazyLoad: false,
 }
 
-describe('ag/Listings/MobileListing', () => {
-  describe('ctaButtons', () => {
-    let wrapper
-    let ctaClick
-    let cardClick
+const singleFamilyProps = {
+  ...props,
+  listing: {
+    ...props.listing,
+    banners: [],
+    singleFamily: true,
+  },
+  ctaButtons: [
+    {
+      children: 'Check Availability',
+      onClick: () => { },
+      className: 'contact',
+    },
+  ],
+}
 
-    beforeEach(() => {
-      ctaClick = jest.fn()
-      cardClick = jest.fn()
-      wrapper = shallow(
-        <MobileListing
-          {...props}
-          ctaButtons={[{ children: 'foo', onClick: ctaClick }, { children: 'bar', onClick: ctaClick }]}
-          onClick={cardClick}
-          isActive
-        />
-      )
-    })
-
-    it('does not render a cta button if there is no text to display in the button', () => {
-      wrapper = shallow(
-        <MobileListing
-          {...props}
-          ctaButtons={[{ onClick: ctaClick }, { children: 'bar', onClick: ctaClick }]}
-          onClick={cardClick}
-          isActive
-        />
-      )
-      expect(wrapper.find(Button).length).toEqual(1)
-    })
-
-    it('handle an array of cta buttons', () => {
-      expect(wrapper.find(Button)).toHaveLength(2)
-    })
-
-    it('fire cta buttons on click action on click', () => {
-      wrapper.find('[data-tid="cta-button"]').at(0).simulate('click')
-      wrapper.find('[data-tid="cta-button"]').at(1).simulate('click')
-      expect(ctaClick.mock.calls).toHaveLength(2)
-    })
-
-    it('fires cta buttons on click action when isActive is false', () => {
-      wrapper = shallow(
-        <MobileListing
-          {...props}
-          ctaButtons={[{ children: 'foo', onClick: ctaClick }, { children: 'bar', onClick: ctaClick }]}
-          onClick={cardClick}
-          isActive={false}
-        />)
-      wrapper.find('[data-tid="cta-button"]').at(0).simulate('click')
-      wrapper.find('[data-tid="cta-button"]').at(1).simulate('click')
-      expect(cardClick.mock.calls).toHaveLength(2)
-    })
-  })
-
-  it('fires favoriteButton click action on favorite button click', () => {
-    const favoriteClick = jest.fn()
+describe('MobileListing', () => {
+  it('passes through custom props', () => {
     const wrapper = mount(
       <MobileListing
         {...props}
-        favoriteButton={{ onClick: favoriteClick }}
+        isActive
+        data-tid="foo"
       />
     )
-    wrapper.find(ToggleButton).simulate('click')
-    expect(favoriteClick).toHaveBeenCalledWith(baseListing, true)
+    expect(wrapper.find('Listing').prop('data-tid')).toEqual('foo')
   })
 
-  it('does not fire cardClick on favorite button click', () => {
-    const cardClick = jest.fn()
-    const wrapper = mount(
-      <MobileListing
-        {...props}
-        onClick={cardClick}
-      />
-    )
-    wrapper.find(ToggleButton).simulate('click')
-    expect(cardClick).not.toHaveBeenCalled()
-  })
+  describe('render listing info', () => {
+    describe('components render', () => {
+      it('renders Price component', () => {
+        const wrapper = mount(<MobileListing {...props} />)
+        expect(wrapper.find(ListingComponents.Price).exists()).toBeTruthy()
+      })
 
-  it('does not fire cardClick on carousel nav click', () => {
-    const cardClick = jest.fn()
-    const wrapper = mount(
-      <MobileListing
-        {...props}
-        onClick={cardClick}
-      />
-    )
+      it('renders PropertyName component', () => {
+        const wrapper = mount(<MobileListing {...props} />)
+        expect(wrapper
+          .find(ListingComponents.PropertyName).exists())
+          .toBeTruthy()
+      })
 
-    wrapper
-      .find('[data-tid="carousel"]')
-      .find('[className="image-gallery-left-nav"]')
-      .simulate('click')
-    expect(cardClick).not.toHaveBeenCalled()
-  })
+      it('renders Bedroom component', () => {
+        const wrapper = mount(<MobileListing {...props} />)
+        expect(wrapper.find(ListingComponents.Bedroom).exists()).toBeTruthy()
+      })
 
-  it('does not fire cardClick on cta click', () => {
-    const cardClick = jest.fn()
-    const wrapper = mount(
-      <MobileListing
-        {...props}
-        onClick={cardClick}
-      />
-    )
-    wrapper.find('[data-tid="cta-button"]').at(0).simulate('click')
-    expect(cardClick).not.toHaveBeenCalled()
-  })
+      it('renders UnitLevelAvailability component', () => {
+        const wrapper = mount(<MobileListing {...props} />)
+        expect(wrapper
+          .find(ListingComponents.UnitLevelAvailability).exists())
+          .toBeTruthy()
+      })
 
-  it('fires onClick on card click', () => {
-    const cardClick = jest.fn()
-    const wrapper = mount(
-      <MobileListing
-        {...props}
-        index={0}
-        onClick={cardClick}
-      />
-    )
-    wrapper.simulate('click')
-    expect(cardClick).toHaveBeenCalled()
-    expect(cardClick.mock.calls).toEqual([[0, baseListing]])
-  })
-
-  it('sets the favorite button to favorited when the listing has isFavorited as true', () => {
-    const favoritedProps = {
-      ...props,
-      listing: {
-        ...props.listing,
-        isFavorited: true,
-      },
-    }
-    const wrapper = shallow(<MobileListing {...favoritedProps} />)
-    expect(wrapper.find(ToggleButton).prop('value')).toBeTruthy()
-  })
-
-  it('sets the favorite button to not favorited when the listing has isFavorited as false', () => {
-    const favoritedProps = {
-      ...props,
-      listing: {
-        ...props.listing,
-        isFavorited: false,
-      },
-    }
-    const wrapper = shallow(<MobileListing {...favoritedProps} />)
-    expect(wrapper.find(ToggleButton).prop('value')).toBeFalsy()
-  })
-
-  describe('photos', () => {
-    it('renders a component with a PhotoCarousel if active', () => {
-      const wrapper = shallow(<MobileListing {...props} />)
-
-      expect(wrapper.find(ListingComponents.Photos)).toHaveLength(1)
-      expect(wrapper.find(ListingComponents.Photo)).toHaveLength(1)
+      it('renders Ratings component', () => {
+        const wrapper = mount(<MobileListing {...props} />)
+        expect(wrapper
+          .find(ListingComponents.Ratings).exists())
+          .toBeTruthy()
+      })
     })
 
-    it('renders a component with one background image if inactive', () => {
-      const wrapper = shallow(<MobileListing {...props} isActive={false} />)
+    describe('single family components render', () => {
+      it('renders Price component', () => {
+        const wrapper = mount(<MobileListing {...singleFamilyProps} />)
+        expect(wrapper.find(ListingComponents.Price).exists()).toBeTruthy()
+      })
 
-      expect(wrapper.find(ListingComponents.Photos)).toHaveLength(0)
-      expect(wrapper.find(ListingComponents.Photo)).toHaveLength(1)
-    })
+      it('renders Address component', () => {
+        const wrapper = mount(<MobileListing {...singleFamilyProps} />)
+        expect(wrapper
+          .find(ListingComponents.Address).exists())
+          .toBeTruthy()
+      })
 
-    it('renders a photo component even without a listing photos array', () => {
-      const omittedPhotoProps = omit(props, 'photos')
-      const wrapper = shallow(<MobileListing {...omittedPhotoProps} isActive={false} />)
+      it('renders Bedroom component', () => {
+        const wrapper = mount(<MobileListing {...singleFamilyProps} />)
+        expect(wrapper.find(ListingComponents.Bedroom).exists()).toBeTruthy()
+      })
 
-      expect(wrapper.find(ListingComponents.Photos)).toHaveLength(0)
-      expect(wrapper.find(ListingComponents.Photo)).toHaveLength(1)
+      it('renders Availability component', () => {
+        const wrapper = mount(<MobileListing {...singleFamilyProps} />)
+        expect(wrapper
+          .find(ListingComponents.Availability).exists())
+          .toBeTruthy()
+      })
     })
   })
 })
