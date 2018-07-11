395cf35d9b70806c8403d382754d8f6c05d970cd
Tad Johnston <tjohnston@rentpath.com>
2018-07-11 12:02:57 -0700

    test(warnings): throw error on warning
    affects: @rentpath/react-ui-ag, @rentpath/react-ui-core, @rentpath/react-ui-rent
    
    Throw an error on any sort of React warning.
    Includes:
    - invalid props
    - required props
    -
    props injected into the DOM that aren't DOM attributes

--- packages/react-ui-ag/test/setup.js
+++ packages/react-ui-ag/test/setup.js
@@ -4,3 +4,14 @@ import enzyme from 'enzyme'
 // react 16 enzyme adapter
 enzyme.configure({ adapter: new Adapter() })
 
+/* eslint-disable no-console */
+const error = console.error
+
+// throw an error on react warnings so developers fix the warnings
+console.error = (message, ...args) => {
+  if (/(Invalid prop|Failed prop type|React does not recognize the `.*?` prop)/gi.test(message)) {
+    throw new Error(message)
+  }
+
+  error.apply(console, [message, ...args])
+}
