cf066a7e506b99469e15c99d95a642b347c37d64
Micah Neumark <mneumark@rentpath.com>
2018-08-09 12:11:59 -0700

    removed themed from Message

--- packages/react-ui-core/src/Form/Message.js
+++ packages/react-ui-core/src/Form/Message.js
@@ -1,14 +1,11 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import themed from 'react-themed'
 import Field from './Field'
 
-@themed(['Message'], { pure: true })
 export default class Message extends PureComponent {
   static propTypes = {
     name: PropTypes.string,
     type: PropTypes.string,
-    theme: PropTypes.object,
     label: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.object,
@@ -17,15 +14,13 @@ export default class Message extends PureComponent {
   }
 
   static defaultProps = {
-    theme: {},
     name: 'message',
     type: 'textarea',
   }
 
   render() {
-    const { theme, ...props } = this.props
     return (
-      <Field className={theme.Message} {...props} />
+      <Field {...this.props} />
     )
   }
 }
