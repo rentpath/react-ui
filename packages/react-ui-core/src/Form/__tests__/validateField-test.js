79a201cca318a29232b83d6d0253a5ce95ade8d3
Micah Neumark <mneumark@rentpath.com>
2018-08-09 14:05:10 -0700

    switched to shallow for validateField test

--- packages/react-ui-core/src/Form/__tests__/validateField-test.js
+++ packages/react-ui-core/src/Form/__tests__/validateField-test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import validateField from '../validateField'
 import Field from '../Field'
 
@@ -7,36 +7,37 @@ describe('validateField', () => {
   let ValidatedField
 
   beforeEach(() => {
-    ValidatedField = validateField(Field)
+    const ThemedValidatedField = validateField(Field)
+    ValidatedField = ThemedValidatedField.WrappedComponent
   })
 
   it('displays the validFieldIcon when the isValid prop is true', () => {
-    const wrapper = mount(<ValidatedField validFieldIcon={<div data-tid="valid-field-icon" />} isValid />)
+    const wrapper = shallow(<ValidatedField validFieldIcon={<div data-tid="valid-field-icon" />} isValid />)
     expect(wrapper.find('[data-tid="valid-field-icon"]')).toHaveLength(1)
   })
 
   it('does not display validFieldIcon when the isValid prop is false', () => {
-    const wrapper = mount(<ValidatedField validFieldIcon={<div data-tid="valid-field-icon" />} isValid={false} />)
+    const wrapper = shallow(<ValidatedField validFieldIcon={<div data-tid="valid-field-icon" />} isValid={false} />)
     expect(wrapper.find('[data-tid="valid-field-icon"]')).toHaveLength(0)
   })
 
   it('passes "valid" to the variant prop of the component when the isValid prop is true', () => {
-    const wrapper = mount(<ValidatedField isValid />)
+    const wrapper = shallow(<ValidatedField isValid />)
     expect(wrapper.find(Field).prop('variant')).toEqual('valid')
   })
 
   it('passes "invalid" to the variant prop of the component when the isValid prop is false', () => {
-    const wrapper = mount(<ValidatedField isValid={false} />)
+    const wrapper = shallow(<ValidatedField isValid={false} />)
     expect(wrapper.find(Field).prop('variant')).toEqual('invalid')
   })
 
   it('displays the validationMessage prop when it exists', () => {
-    const wrapper = mount(<ValidatedField validationMessage="foo bar" />)
+    const wrapper = shallow(<ValidatedField validationMessage="foo bar" />)
     expect(wrapper.find('[data-tid="validation-message"]').text()).toEqual('foo bar')
   })
 
   it('does not render the validationMessage prop when it does not exist', () => {
-    const wrapper = mount(<ValidatedField />)
+    const wrapper = shallow(<ValidatedField />)
     expect(wrapper.find('[data-tid="validation-message"]')).toHaveLength(0)
   })
 })
