66f3e7f0713b997cc187302aad99e3499d7c833d
Pat Ostler <ostlerp@gmail.com>
2018-10-01 17:03:13 -0400

    feat(Markers): add ability to specify scale of marker icons
    affects: @rentpath/react-ui-ag
    
    As part of the feature definition, an `iconScale` can be specified to scale the icon
    By default,
    scale is 1

--- packages/react-ui-ag/src/Gmap/__tests__/Markers-test.js
+++ packages/react-ui-ag/src/Gmap/__tests__/Markers-test.js
@@ -64,6 +64,59 @@ describe('ag/Markers', () => {
       expect(instance.marker(feature).zIndex).toEqual(1)
     })
 
+    it('uses the iconScale for icon function', () => {
+      const scale = 0.5
+      const feature = {
+        properties: {
+          iconScale: scale,
+        },
+      }
+
+      const markerIcon = jest.fn()
+      const wrapper = shallow(<Markers map={map} markerIcon={markerIcon} />)
+
+      wrapper.instance().marker(feature)
+
+      expect(markerIcon).toHaveBeenCalledTimes(1)
+      expect(markerIcon).toHaveBeenCalledWith(scale)
+    })
+
+    it('uses the iconScale for isBasic properties', () => {
+      const scale = 0.75
+      const feature = {
+        properties: {
+          isBasic: true,
+          iconScale: scale,
+        },
+      }
+
+      const markerInactiveIcon = jest.fn()
+      const wrapper = shallow(<Markers map={map} markerInactiveIcon={markerInactiveIcon} />)
+
+      wrapper.instance().marker(feature)
+
+      expect(markerInactiveIcon).toHaveBeenCalledTimes(1)
+      expect(markerInactiveIcon).toHaveBeenCalledWith(scale)
+    })
+
+    it('uses the iconScale for hover', () => {
+      const scale = 1.25
+      const feature = {
+        properties: {
+          iconScale: scale,
+        },
+      }
+
+      const markerIconHover = jest.fn()
+      const wrapper = shallow(<Markers map={map} markerIconHover={markerIconHover} />)
+      const marker = wrapper.instance().marker(feature)
+
+      marker.onMouseOver('mouse_over', null, window.google.maps.Marker())
+
+      expect(markerIconHover).toHaveBeenCalledTimes(1)
+      expect(markerIconHover).toHaveBeenCalledWith(scale)
+    })
+
     it('applies extra props correctly when passed as a `marker` prop', () => {
       const markerProps = { icon: blackDotIconWithBalloon() }
       const wrapper = shallow(<Markers map={map} marker={() => (markerProps)} />)
