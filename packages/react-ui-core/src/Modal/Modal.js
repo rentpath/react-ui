901c2c2a2748a5a10df356e1b43a26bee081bf43
Christopher Tse <ctse@rentpath.com>
2021-09-09 12:37:12 -0500

    fix overlay lock class toggle
    [AG-6453](https://rentpath.atlassian.net/browse/AG-6453)

--- packages/react-ui-core/src/Modal/Modal.js
+++ packages/react-ui-core/src/Modal/Modal.js
@@ -44,7 +44,7 @@ export default class Modal extends PureComponent {
   }
 
   componentDidMount() {
-    if (this.props.lockBodyScrolling) {
+    if (this.props.lockBodyScrolling && this.props.isOpen) {
       this.toggleBodyClass(true)
     }
   }
